3541c73d844e3ef536d7f6f1bfe3c53b497b5bbd
Anushka <nush3bhr@gmail.com>
2021-01-18 23:19:18 +0530

    corrected switch case and optimised code

--- src/Components/FunVdSwitch.js
+++ src/Components/FunVdSwitch.js
@@ -7,23 +7,32 @@ const FunVdSwitch = () => {
     setString(event.target.value);
   };
   const getLetter = (s) => {
-    let alpha;
     switch (
       s.charAt(0) //we can also us s[0]
     ) {
-      case "a" || "e" || "i" || "o" || "u":
-        alpha = "A";
-        break;
-      case "b" || "c" || "d" || "f" || "g":
-        alpha = "B";
-        break;
-      case "h" || "j" || "k" || "l" || "m":
-        alpha = "C";
-        break;
+      case "a":
+      case "e":
+      case "i":
+      case "o":
+      case "u":
+        return "A";
+
+      case "b":
+      case "c":
+      case "d":
+      case "f":
+      case "g":
+        return "B";
+
+      case "h":
+      case "j":
+      case "k":
+      case "l":
+      case "m":
+        return "C";
       default:
-        alpha = "D";
+        return "D";
     }
-    return alpha;
   };
   const handleSubmit = (event) => {
     event.preventDefault();
